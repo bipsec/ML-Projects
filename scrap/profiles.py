44b5abc462cb68acf7836d5bccf69c3548ec1077
bipsec <bip.sec22@gmail.com>
2024-05-09 11:54:23 +0900

    profiles data scraped

--- scrap/profiles.py
+++ scrap/profiles.py
@@ -11,7 +11,9 @@ def scrape_profile_data(url):
 
         data = []
         for profile in profiles:
-            name = profile.find('h1').text.strip().split()
+            get_prof_name = profile.find('h1').text.strip().split()
+            name = " ".join(item for item in get_prof_name)
+
             email_image = profile.find('img', class_='email-image')
             if email_image:
                 email_text = email_image.get('alt', '').split('email', 1)[
@@ -28,7 +30,7 @@ def scrape_profile_data(url):
             else:
                 research_text = ''
 
-            data.append([name, email, research_text ])
+            data.append([name, email, research_text])
 
         return data
     except Exception as e:
@@ -43,14 +45,12 @@ def save_to_csv(data, filename='profile_data.csv'):
 if __name__ == "__main__":
     base_url = 'https://cs.txstate.edu/accounts/profiles/'
     profile_data = scrape_profile_data(base_url)
-    profile_arr = ["hs15/", "ma04/", "t_i24/", "sox16/", "ok11/"]
+    profile_arr = ["hs15/", "ma04/", "t_i24/", "sox16/", "ok11/", "zz11/", "k_y47/", "j_t463/", "js236/", "vgt16/",
+                   "asf93/", "aq10/", "rp31/", "wp01/", "ueg11/", "hn12/", "tgo10/", "v_m137/", "t_l81/", "fsr11/",
+                   "ok11/", "lk04/", "sox16/", "t_i24/", "ch01/", "mg65/", "hag10/", "jg66/", "xc10/", "mb92/", "ma04/"]
     scraped_data = []
     for item in profile_arr:
         url = base_url + item
         text_data = scrape_profile_data(url)
-        if text_data:
-            # cleaned_data = [[item.strip().replace('\n', '') for item in profile] for profile in text_data]
-            scraped_data.extend(text_data)
-
-    if scraped_data:
+        scraped_data.extend(text_data)
         save_to_csv(scraped_data)
